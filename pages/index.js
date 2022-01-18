a148ef8f130650093d119b21f7721e9fd32fbe0d
keisasaki3 <keisasaki6@gmail.com>
2022-01-18 12:45:20 +0900

    dev branch

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{siteTitle} (Dev Branch)</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
